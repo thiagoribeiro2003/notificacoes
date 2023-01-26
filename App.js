ee70c560dfbadda9e8703b6c23727f19a7f6d2c1
thiagoribeiro2003 <thiagoribeiro20003@gmail.com>
2023-01-26 09:06:08 -0300

    Funções de notificação

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, SafeAreaView, Button } from "react-native";
+import { useEffect } from "react";
+import * as Notifications from "expo-notifications";
 
 export default function App() {
+  useEffect(() => {
+    /* Ouvinte de evento para as notificações recebidas, ou seja, 
+    quando a notificação aparece no topo da tela do dispositivo. */
+    Notifications.addNotificationReceivedListener();
+
+    /* Ouvinte de evento para as respostas dadas ás Notificações, ou seja, 
+    quando o usuário interage (toca na notificação). */
+    Notifications.addNotificationResponseReceivedListener();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar />
+      <SafeAreaView style={estilos.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Button title="Disparar notificação" />
+      </SafeAreaView>
+    </>
   );
 }
 
-const styles = StyleSheet.create({
+const estilos = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
